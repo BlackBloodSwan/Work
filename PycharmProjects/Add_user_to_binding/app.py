8d45143b020803a74f66b1cec22260c963f2a3d2
Domenic Schalla <schallado@mediamarktsaturn.de>
2019-12-19 10:57:26 +0100

    last

--- PycharmProjects/Add_user_to_binding/app.py
+++ PycharmProjects/Add_user_to_binding/app.py
@@ -89,5 +89,5 @@ def set_policy(credentials, project_id, policy):
 
 
 if __name__ == '__main__':
-    app.run( debug=True)
-    #app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    #app.run( debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
